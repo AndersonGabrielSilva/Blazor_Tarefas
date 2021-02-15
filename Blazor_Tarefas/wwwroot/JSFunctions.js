c3090455ef84e1f9399ffbc85080c78adf80d3db
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-15 16:50:12 -0300

    Invocando Metodos em de Instancia do C# com JavaScript

--- Blazor_Tarefas/wwwroot/JSFunctions.js
+++ Blazor_Tarefas/wwwroot/JSFunctions.js
@@ -2,3 +2,18 @@
     alert(message);
 }
 
+//Forma de chamar um metodo estatico do C# pelo JavaScript
+function getTotalTarefas() {
+    DotNet.invokeMethodAsync("Blazor_Tarefas", "ObterTotalTarefa")
+        .then(resultado => {
+            alert("Total de Tarefas : " + resultado);
+        });
+}
+
+function getTotalTarefasIntancia(dotnet) {
+    return dotnet.invokeMethodAsync("ObterTotalTarefasInstancia")
+        .then(resultado => {
+            alert("Total de Tarefas : " + resultado);
+        });
+}
+
