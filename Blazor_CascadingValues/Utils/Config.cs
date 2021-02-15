86e46aebceaed0136d812085408bdeed0b35331a
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-15 18:19:26 -0300

    Uilizando o Cascading Values Como passar Valores em cascata para os componentes filhos

---
+++ Blazor_CascadingValues/Utils/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_CascadingValues.Utils
+{
+    public class Config
+    {
+
+        public string CorDeFundo { get; set; } = "blue";
+
+        public string TamanhoFonte { get; set; } = "60px";
+    }
+}
