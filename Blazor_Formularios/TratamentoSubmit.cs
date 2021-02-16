5bca4dfee8a49e58690fc20122851a27b3b7e4af
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-16 13:33:19 -0300

    Utilizando o OnValidSubmit e OnInvalidSubmit.

---
+++ Blazor_Formularios/TratamentoSubmit.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Blazor_Formularios
+{
+   
+    /*
+       * Tratamento do Submit:
+        
+        - OnSubmit :
+            Ocorre quando postamos o formulario enviando os dados para o servidor (permite enviar sempre).
+
+        - OnValidSubmit :
+            Ocorre quando o usuário posta um formulario válido. (deve ser usado com o componente DataAnnotationsValidator).
+
+        - OnInvalidSubmit :
+            Ocorre quando o usuário tentou postar um formulario que não respeita as regras de validação.
+
+        * Cada um destes eventos passa um EditContext como um parâmetro, que podemos usar para determinar 
+          o status da entrada do usuário.
+     */
+
+
+
+}
