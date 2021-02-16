857f281032740237ae31c68d0e7fcf034bfb54af
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-16 12:58:51 -0300

    Utilizando DataAnotation para validação

--- Blazor_Formularios/Models/Usuario.cs
+++ Blazor_Formularios/Models/Usuario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,8 +9,14 @@ namespace Blazor_Formularios.Models
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required]
         public string Nome { get; set; }
+
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Range(18,80, ErrorMessage = "A idade deve estar entre {0} e {1}")]
         public int Idade { get; set; }
     }
 }
