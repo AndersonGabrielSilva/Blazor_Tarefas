d1b9d89a0561c164d1de7daa2dd70e23859ee994
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-16 00:07:53 -0300

    Salvando informaçoes no Local Storage

---
+++ Blazor_LocalStorage/LocalStorage.cs
@@ -0,0 +1,25 @@
+﻿
+
+namespace Blazor_Tarefas
+{
+    /*
+     
+--------------------------------------------------------------------------------------------------------------------------     
+        
+     * Local Storage : O local Storage permite amazenar dados de forma simples e sem expiração, 
+             ou seja os dados ficam armazenados enquanto não forem apagados quer seja via código ou pelo 
+             próprio navegador.
+
+
+        * Session Storage: é muito parecido com o localStorage com a diferença de que no session Storage 
+            os dados são limpos ao expirar a sessão da página, ou seja, quando a página (aba ou janela) 
+            for fechada.        
+
+--------------------------------------------------------------------------------------------------------------------------
+                 
+                 Não armazenar dados sensíveis ou críticos nestes locais. Uma boa pratica é armazenar
+                 dados cifrados e para isso podemos usar os recursos do Asp.Net para proteço de dados
+
+     */
+
+}
