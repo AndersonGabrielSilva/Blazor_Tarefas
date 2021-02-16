15f90501e9c8e1f83580382864e9e3d4738b0aad
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-16 12:21:50 -0300

    Comnponente EditForm e InputText. como utilizar o atributo Model

---
+++ Blazor_Formularios/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor_Formularios.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Idade { get; set; }
+    }
+}
