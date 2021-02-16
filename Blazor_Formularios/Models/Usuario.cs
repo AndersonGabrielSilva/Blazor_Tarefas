5d030867c46f1c859804025c3756e5478cd8699b
Anderson Gabriel Silva <andersongabriel_maria@hotmail.com>
2021-02-16 13:14:52 -0300

    Validation Sumary e Validatition Message

--- Blazor_Formularios/Models/Usuario.cs
+++ Blazor_Formularios/Models/Usuario.cs
@@ -10,10 +10,12 @@ namespace Blazor_Formularios.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="O nome é obrigatorio")]
+        [StringLength(10,ErrorMessage ="O nome não pode exceder 10 caracteres")]
         public string Nome { get; set; }
 
-        [EmailAddress]
+        [EmailAddress(ErrorMessage ="O formato do email é inválido")]
+        [Required(ErrorMessage = "O email é Obrigatorio")]
         public string Email { get; set; }
 
         [Range(18,80, ErrorMessage = "A idade deve estar entre {0} e {1}")]
